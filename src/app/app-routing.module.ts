5be8612fda81e33b791a552e6ac4a2fc077b619e
JessCP25 <jpayanoc25@gmail.com>
2024-10-12 20:14:32 -0500

    Usando switchMap para dos solicitudes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
     component: HomeComponent,
   },
   {
-    path: 'category',
+    path: 'category/:id',
     component: CategoryComponent,
   },
   {
--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Product } from 'src/app/models/product.model';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.scss']
+  styleUrls: ['./category.component.scss'],
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnInit {
+  categoryId: string | null = null;
+  limit = 10;
+  offset = 0;
+  products: Product[] = [];
 
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductsService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap
+      .pipe(
+        switchMap((paramMap) => {
+          this.categoryId = paramMap.get('id');
+          if (this.categoryId !== null) {
+            return this.productService.getByCategory(
+              this.categoryId!,
+              this.limit,
+              this.offset
+            );
+          }
+          return [];
+        })
+      )
+      .subscribe((res) => {
+        this.products = res;
+      });
+  }
+
+  // getProducts() {
+  //   this.productService
+  //     .getByCategory(this.categoryId!, this.limit, this.offset)
+  //     .subscribe({
+  //       next: (res) => {
+  //         this.products = res;
+  //       },
+  //     });
+  // }
+
+  loadMore() {
+    this.productService
+      .getByCategory(this.categoryId!, this.limit, this.offset)
+      .subscribe((data) => {
+        this.products = this.products.concat(data);
+        this.offset += this.limit;
+      });
+  }
 }
