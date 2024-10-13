14176fdae6a07f90bbc650338ca12f645ba3e571
JessCP25 <jpayanoc25@gmail.com>
2024-10-12 20:30:03 -0500

    Uso de routerLink

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { StoreService } from '../../services/store.service'
 import { AuthService } from '../../services/auth.service';
 import { User } from '../../models/user.model';
+import { Category } from 'src/app/models/category.model';
+import { CategoriesService } from 'src/app/services/categories.service';
 
 @Component({
   selector: 'app-nav',
@@ -14,16 +16,19 @@ export class NavComponent implements OnInit {
   activeMenu = false;
   counter = 0;
   profile: User | null = null;
+  categories: Category[] = [];
 
   constructor(
     private storeService: StoreService,
     private authService: AuthService,
+    private categoryService: CategoriesService
   ) { }
 
   ngOnInit(): void {
     this.storeService.myCart$.subscribe(products => {
       this.counter = products.length;
     });
+    this.getCategories();
   }
 
   toggleMenu() {
@@ -37,4 +42,11 @@ export class NavComponent implements OnInit {
     });
   }
 
+  getCategories(){
+    this.categoryService.getAll().subscribe({
+      next: (data) => {
+        this.categories = data;
+      }
+    })
+  }
 }
