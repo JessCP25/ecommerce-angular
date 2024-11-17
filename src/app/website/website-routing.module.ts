ed77ec9eadca787741e07996f2fc8a01e39c9d5c
JessCP25 <jpayanoc25@gmail.com>
2024-11-17 16:36:01 -0500

    Feat: Creando un guard

--- src/app/website/website-routing.module.ts
+++ src/app/website/website-routing.module.ts
@@ -8,6 +8,8 @@ import { RegisterComponent } from './pages/register/register.component';
 import { RecoveryComponent } from './pages/recovery/recovery.component';
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 import { LayoutComponent } from './components/layout/layout.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { AuthGuard } from '../guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -50,6 +52,11 @@ const routes: Routes = [
         path: 'recovery',
         component: RecoveryComponent,
       },
+      {
+        path: 'profile',
+        canActivate: [AuthGuard],
+        component:  ProfileComponent,
+      }
     ],
   },
 ];
