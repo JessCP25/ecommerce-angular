3f8fbabdced31afb9c5aaa7210226b0fbdf40c21
JessCP25 <jpayanoc25@gmail.com>
2024-11-17 20:12:21 -0500

    Feat: Agregando un CanDesactivate

--- src/app/website/pages/register/register.component.ts
+++ src/app/website/pages/register/register.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { OnExit } from 'src/app/guards/exit.guard';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnExit {
+
+  onExit(){
+    const rta = confirm('¿Estás seguro de salir, se perderan los datos?');
+    return rta;
+  }
 
 }
